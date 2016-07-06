55adb5f6c77946278f4a45b6a101593a335788fd
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-07-07 00:00:48 +0200

    Fix build system
    Also enable HTTP cache for images

---
+++ app/src/main/java/edu/stanford/thingengine/engine/ThingEngineApplication.java
@@ -0,0 +1,27 @@
+package edu.stanford.thingengine.engine;
+
+import android.app.Application;
+import android.net.http.HttpResponseCache;
+import android.util.Log;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by gcampagn on 7/6/16.
+ */
+public class ThingEngineApplication extends Application {
+    public static final String LOG_TAG = "thingengine";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        try {
+            long httpCacheSize = 10 * 1024 * 1024; // 10 MiB
+            HttpResponseCache.install(new File(getCacheDir() + "/http"), httpCacheSize);
+        } catch(IOException e) {
+            Log.w(LOG_TAG, "Failed to enable HTTP cache, application will run slower", e);
+        }
+    }
+}
