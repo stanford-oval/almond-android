ef36443c2db0d94b5da5dd675770a6ba1f476e59
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-07-12 23:48:33 +0200

    Fix build with no babel plugins
    We need to install them. All of them.

--- app/build_js.sh
+++ app/build_js.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
+BABEL_PLUGINS="
+babel-preset-es2015
+babel-plugin-transform-es2015-template-literals
+babel-plugin-transform-es2015-literals
+babel-plugin-transform-es2015-function-name
+babel-plugin-transform-es2015-arrow-functions
+babel-plugin-transform-es2015-block-scoped-functions
+babel-plugin-transform-es2015-classes
+babel-plugin-transform-es2015-object-super
+babel-plugin-transform-es2015-shorthand-properties
+babel-plugin-transform-es2015-duplicate-keys
+babel-plugin-transform-es2015-computed-properties
+babel-plugin-transform-es2015-for-of
+babel-plugin-transform-es2015-sticky-regex
+babel-plugin-transform-es2015-unicode-regex
+babel-plugin-check-es2015-constants
+babel-plugin-transform-es2015-spread
+babel-plugin-transform-es2015-parameters
+babel-plugin-transform-es2015-destructuring
+babel-plugin-transform-es2015-block-scoping
+babel-plugin-transform-es2015-typeof-symbol
+babel-plugin-transform-es2015-modules-commonjs
+babel-plugin-transform-regenerator"
+
 install_babel() {
 	test -d "$projectdir/node_modules" || mkdir ../node_modules
-	(cd "$projectdir/" ; npm install babel-cli babel-preset-es2015 )
+	(cd "$projectdir/" ; npm install babel-cli $BABEL_PLUGINS )
 }
 
 
