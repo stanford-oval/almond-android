277333dced94c6f0ac3d196e6cb17834d10cfd2f
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-03-07 13:24:04 -0800

    Update Copyright notices
    After rereading the University Copyright Agreement, it turns out
    that the university owns the copyright of this work (because it
    is sponsored research). This has been the case from the beginning,
    and previous copyright notices were factually wrong, so this is
    not a change or a relicensing.

--- jsapp/build_translations.js
+++ jsapp/build_translations.js
@@ -1,3 +1,14 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of ThingEngine
+//
+// Copyright 2017 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
 const gettextParser = require('gettext-parser');
 const fs = require('fs');
 
