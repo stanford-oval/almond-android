704bc6a2e74bb7df20f6b14aefe0c984549a34ea
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-05-02 11:07:46 -0700

    rebuild-libraries.sh: be quieter

--- rebuild-libraries.sh
+++ rebuild-libraries.sh
@@ -10,7 +10,7 @@ export AR_host=${AR:-ar}
 
 ANDROID_NDK_ROOT=${ANDROID_NDK_ROOT:-~/Android/android-ndk-r16b}
 
-test -d ${ANDROID_NDK_ROOT} || ( mkdir -p ~/Android ; cd ~/Android ; wget https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip ; unzip android-ndk-r16b-linux-x86_64.zip )
+test -d ${ANDROID_NDK_ROOT} || ( mkdir -p ~/Android ; cd ~/Android ; wget https://dl.google.com/android/repository/android-ndk-r16b-linux-x86_64.zip ; unzip -q android-ndk-r16b-linux-x86_64.zip )
 
 export CFLAGS="-g -O2 -fPIC -fexceptions"
 export CXXFLAGS="-g -O2 -fPIC -std=c++14 -fexceptions"
