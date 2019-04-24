30779c311ac48e06bf7d42f169fb7dc026d84d5b
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-04-24 13:53:17 -0700

    Remove cvc4 from the "build" system

--- rebuild-libraries.sh
+++ rebuild-libraries.sh
@@ -22,22 +22,6 @@ test -d ${download} || mkdir -p ${download}
 export OUT=${OUT:-./app/src/main/jniLibs}
 
 download_all() {
-# Step 1: boost
-test -f ${download}/boost_1_65_1.tar.bz2 || wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.bz2 -O ${download}/boost_1_65_1.tar.bz2
-test -d ${download}/boost_1_65_1 || ( tar -xvf ${download}/boost_1_65_1.tar.bz2 -C ${download} ; cd ${download}/boost_1_65_1 ; ./bootstrap.sh )
-
-test -f ${download}/gmp-6.1.2.tar.xz || wget https://ftp.gnu.org/gnu/gmp/gmp-6.1.2.tar.xz -O ${download}/gmp-6.1.2.tar.xz
-test -d ${download}/gmp-6.1.2 || tar -xvf ${download}/gmp-6.1.2.tar.xz -C ${download}
-
-test -f ${download}/cvc4-1.5.tar.gz || wget http://cvc4.cs.stanford.edu/downloads/builds/src/cvc4-1.5.tar.gz -O ${download}/cvc4-1.5.tar.gz
-test -d ${download}/cvc4-1.5 || (
-tar -xvf ${download}/cvc4-1.5.tar.gz -C ${download} ;
-cd ${download}/cvc4-1.5 ;
-./contrib/get-antlr-3.4 ;
-cd antlr-3.4/src/libantlr3c-3.4 ;
-make distclean
-autoreconf -fvi )
-
 test -d ${download}/node || git clone https://github.com/stanford-oval/node.git ${download}/node
 (cd ${download}/node ; git checkout 74941eb33c983b2a1a4d6bfc3311bcfb44958357 )
 
@@ -84,38 +68,11 @@ touch ${prefix}/include/fpu_control.h
 # the python in the toolchain is broken, remove it
 rm -fr ${this_dir}/build-$arch/toolchain/bin/python* ${this_dir}/build-$arch/toolchain/lib/python2.7 ${this_dir}/build-$arch/toolchain/lib/libpython2.7.a
 
-export ANTLR=`pwd`/download/cvc4-1.5/antlr-3.4/bin/antlr3
 export PATH=`pwd`/build-$arch/toolchain/bin:${prefix}/bin:${base_PATH}
 export CPATH="${prefix}/include"
 export LDFLAGS="-L${prefix}/lib -Wl,--build-id=sha1"
 
-# Step 2: boost
-( cd ${download}/boost_1_65_1 ;
-./b2 -j8 toolset=clang-android$arch link=shared runtime-link=shared threading=multi variant=release target-os=android architecture=${arch} --without-python install --prefix=${prefix} )
-
-# Step 3: gmp
-mkdir -p ${build}/gmp
-( cd ${build}/gmp ;
-test -f ./config.status || ${download}/gmp-6.1.2/configure CC=${triple}-clang CXX=${triple}-clang++ --prefix=${prefix} --host=${triple} --build=x86_64-linux-gnu --enable-cxx --disable-shared --without-readline --with-pic )
-make -j8 -C ${build}/gmp
-make -C ${build}/gmp install
-
-# Step 4: antlr
-mkdir -p ${build}/antlr3
-( cd ${build}/antlr3 ;
-test -f ./config.status || ${download}/cvc4-1.5/antlr-3.4/src/libantlr3c-3.4/configure CC=${triple}-clang CXX=${triple}-clang++ --prefix=${prefix} --host=${triple} --build=x86_64-linux-gnu --disable-shared --disable-antlrdebug --with-pic
-)
-make -j8 -C ${build}/antlr3 CFLAGS="${CFLAGS} -I${download}/cvc4-1.5/antlr-3.4/src/libantlr3c-3.4/include"
-make -C ${build}/antlr3 install
-
-# Step 5: cvc4
-mkdir -p ${build}/cvc4
-( cd ${build}/cvc4 ;
-test -f ./config.status || ${download}/cvc4-1.5/configure CC=${triple}-clang CXX=${triple}-clang++ --prefix=${prefix} --host=${triple} --build=x86_64-linux-gnu --enable-optimized --disable-doxygen-doc --disable-replay --with-gmp --with-boost=${prefix} --without-readline --disable-thread-support )
-make -j8 -C ${build}/cvc4 CFLAGS="${CFLAGS}" CXXFLAGS="${CXXFLAGS} -Dfgetc_unlocked=fgetc"
-make -C ${build}/cvc4 install
-
-# Step 6: node
+# Step 2: node
 export CC=${this_dir}/build-$arch/toolchain/bin/$triple-clang
 export CXX=${this_dir}/build-$arch/toolchain/bin/$triple-clang++
 export LINK=${this_dir}/build-$arch/toolchain/bin/$triple-clang++
@@ -129,9 +86,9 @@ test -d ${build}/node || git clone ${download}/node ${build}/node
 test -f ./config.gypi || ./configure --shared --dest-cpu=${nodejs_cpu} --dest-os=android --without-snapshot --openssl-no-asm --with-intl=small-icu --without-inspector )
 make -j8 -C ${build}/node/out CC.host=${CC_host} CXX.host=${CXX_host} LINK.host=${LINK_host} AR.host=${AR_host}
 
-# Step 7: copy the libraries in the right place
+# Step 3: copy the libraries in the right place
 mkdir -p ${OUT}/${android_arch}
-cp ${prefix}/lib/libcvc4.so ${prefix}/lib/libcvc4parser.so ${build}/node/out/Release/lib.target/libnode.so ${OUT}/${android_arch}/
+cp ${build}/node/out/Release/lib.target/libnode.so ${OUT}/${android_arch}/
 #${STRIP} ${OUT}/${android_arch}/libcvc4.so ${OUT}/${android_arch}/libcvc4parser.so ${OUT}/${android_arch}/libnode.so 
 
 }
